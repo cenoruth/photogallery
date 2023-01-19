dabec5bf3008e98ce3dc5aa857e2210b458b8b75
Steven Anthony <santhony.n@gmail.com>
2023-01-18 19:45:36 -0800

    update crsf setting

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -160,3 +160,7 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://photogallery-production-6629.up.railway.app/'
+]
