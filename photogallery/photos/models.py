e01ed60aee537d7fa6300b0c2fff992c1b3f5665
Steven Anthony <santhony.n@gmail.com>
2023-01-18 16:40:32 -0800

    first commit

---
+++ photogallery/photos/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+    submitter = models.TextField(default='anonymous')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(null=False, blank=False)
+    # description = models.TextField()
+    uploaded_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.description
---
+++ photogallery/photos/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render, redirect
+from .models import Category, Photo
+from django.contrib.auth import authenticate, login, logout
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
+
+# Create your views here.
+
+def loginUser(request):
+    page = 'login'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('gallery')
+
+    return render(request, 'photos/login_register.html', {'page': page})
+
+def logoutUser(request):
+    logout(request)
+    return redirect('gallery')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+            user = authenticate(request, username=user.username, password=request.POST['password1'])
+
+            if user is not None:
+                login(request, user)
+                return redirect('gallery')
+
+    context = {'form': form, 'page': page}
+    return render(request, 'photos/login_register.html', context)
+
+
+def gallery(request):
+    category = request.GET.get('category')
+    page = request.GET.get('page', 1)
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
+
+    paginator = Paginator(photos, 20)
+    try:
+        photos_page = paginator.page(page)
+    except PageNotAnInteger:
+        photos_page = paginator.page(1)
+    except EmptyPage:
+        photos_page = paginator.page(paginator.num_pages)
+
+    categories = Category.objects.all()
+    context = {'categories': categories, 'photos': photos_page}
+    return render(request, 'photos/gallery.html', context)
+
+def viewPhoto(request, pk):
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo': photo})
+
+def addPhoto(request):
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        if data['category'] != 'none': 
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+
+
+        photo = Photo.objects.create(
+            submitter=data['submitter'],
+            category=category,
+            #description=data['description'],
+            image=image,
+        )
+
+        return redirect('gallery')
+
+    context = {'categories': categories}
+    return render(request, 'photos/add.html', context)
