c786a4de578cfd19baec0bb5b932a2cabb9689fc
Steven Anthony <santhony.n@gmail.com>
2023-01-18 19:33:37 -0800

    staticfiles commit

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = True
 
 #ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
